7d38f490d6f453907401440c71e70927689455d9
ayoub <ayoubousbia02@gmail.com>
2023-07-08 19:48:38 +0200

    google oath with passport js

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 import express from "express"
+import passport from "passport"
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import keys from "./config/key.js"
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({hi : 'there'})
-})
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, 
+    accessToken => {
+        console.log(accessToken)
+    })
+)
 
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+    })
+)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`check at http://localhost:${PORT}`)
+});
