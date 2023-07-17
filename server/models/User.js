f374e39ed885bd0cbf4c87ea0e1be5ae68be6afa
ayoub <ayoubousbia02@gmail.com>
2023-07-17 17:08:52 +0200

    add models folder for mongoose schema

---
+++ server/models/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+const userSchema = new Schema ({
+    googleId: String
+})
+
+
+export default mongoose.model('users', userSchema)
+
+
