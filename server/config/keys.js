e0c633abded57e2c420f45d29a7eeb1a2d33872c
ayoub <ayoubousbia02@gmail.com>
2023-09-07 11:09:31 +0200

    setup environnemnt variable

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,10 +1,12 @@
-const keys = {
-    googleClientID: '636348993460-bdcnuooftslhao96jbusi5n9ibrtsb3b.apps.googleusercontent.com',
-    googleClientSecret: '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',
-    mongoURI: 'mongodb+srv://ayoubousbia99:6TwTDkh1tPrpYHLq@emaily.w1chiwz.mongodb.net/emailydb?retryWrites=true&w=majority',
-    cookieKey: 'fhaujkndsfaowhenajsgoiahsg'
+let config;
 
-    
-};
+if (process.env.NODE_ENV === 'production') {
 
-export default keys
+    config = await import('./prod.js')
+
+} else {
+
+    config = await import('./dev.js')
+}
+
+export default config.default
