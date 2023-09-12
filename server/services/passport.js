ca7ba930f7e3d55dca218b14a5f76b14b6736ced
ayoub <ayoubousbia02@gmail.com>
2023-09-12 19:55:38 +0200

    add async await syntax

--- server/services/passport.js
+++ server/services/passport.js
@@ -22,19 +22,17 @@ passport.use(
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
     }, 
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({googleId: profile.id})
-        .then((existingUser)=> {
-            if (!existingUser) {
-                new User ({ googleId: profile.id }).save()
-                .then(user => done(null, user))
-            } else {
-                done(null, existingUser)
-            }
-        })
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({googleId: profile.id}) 
 
-        
+        if (existingUser) {
+            done(null, existingUser)
+        } else {
+            const user = await new User ({ googleId: profile.id }).save()
+            done(null, user)
+        }
     })
+        
 )
 
 export default passport
