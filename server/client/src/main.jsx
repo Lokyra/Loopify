8b54377c2e6ed0734b2c5ff602821e03ebb5f35f
ayoub <ayoubousbia02@gmail.com>
2023-09-26 13:39:43 +0200

    add component structure & header component start taking form

--- server/client/src/main.jsx
+++ server/client/src/main.jsx
@@ -1,10 +1,20 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import reduxThunk from 'redux-thunk'
+
+
+import App from './components/App'
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 )
